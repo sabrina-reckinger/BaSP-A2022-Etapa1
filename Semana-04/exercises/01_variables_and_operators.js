fc2d4bc0eecba579e8211843495e4db03b4574e3
Sabrina Edith Reckinger <sabrina.reckinger@gmail.com>
2022-09-18 13:40:54 -0300

    BaSP-W04-S2022: Add JS exercises folder

---
+++ Semana-04/exercises/01_variables_and_operators.js
@@ -0,0 +1,23 @@
+// Variables y Operadores
+
+
+// Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable.
+
+var numerOne = 5
+var numberTwo = 10
+addition = numerOne + numberTwo
+console.log ("La suma de ambos números es: ", addition)
+
+// Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
+
+var firstName = "Sabrina"
+var lastName = "Reckinger"
+fullName = firstName + " " + lastName
+console.log ("El  nombre completo de quien esta haciendo estos ejercicios es: ", fullName)
+
+// Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) guardando el resultado de la suma en una 3er variable (utilizar length).
+
+var firstString = "perro"
+var secondString = "gordo"
+longOfPhrase = firstString.length + secondString.length
+console.log ("La cantidad de letras que contiene la frase es: ", longOfPhrase)
